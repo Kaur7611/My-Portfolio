e32f9ebfa2994ba0958fd3497342beb59bcaa879
Kamaljit Kaur <76kaur11@gmail.com>
2025-09-20 12:20:56 +0000

    updated about apge

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "tech related background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-   "I'm a self-taught Python and Flask web developer with a passion for building real-world applications. I enjoy turning ideas into functional, user-friendly websites.";
+  "I'm a self-taught web developer with a passion for building real-world applications. I enjoy turning ideas into functional, user-friendly websites.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
